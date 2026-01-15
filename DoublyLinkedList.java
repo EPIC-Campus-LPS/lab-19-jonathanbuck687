a2eecadea25fe3d752f05bd135f8a5caa5ae9029
jonathanbuck687 <jonathanb138@lpsk12.org>
2026-01-15 11:28:31 -0700

    j

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,14 +1,25 @@
 public class DoublyLinkedList<E> implements List {
     private Node<E> head;
+    private int size = 0;
+    public Node<E> traverse(int i, Node<E> node){
+        if (i == 0) {
+            return node;
+        }
+        return traverse(i - 1, node.next);
+    }
     @Override
     public void add(E value) {
         if (head == null) {
             head == new Node<>(value);
+            size++;
+            return;
         }
-        else {
-            Node<E> newValue = new Node<>(value);
-            Node.setNext(newValue);
-        }
+
+        Node<E> newValue = new Node<>(value);
+        Node<E> current = traverse(size - 1, head);
+        size++
+        newValue = current;
+        newValue.setPrevious(current)
     }
     @Override
     public void add(int i, E element) throws IndexOutOfBoundsException;
@@ -20,4 +31,10 @@ public class DoublyLinkedList<E> implements List {
     public Node<E>  get(int i) throws IndexOutOfBoundsException;
     @Override
     public void set(int i, E element) throws IndexOutOfBoundsException;
+    @Override
+    public int size();
+    @Override
+    public boolean isEmpty();
+    @Override
+    public String toString();
 }
--- Node.java
+++ Node.java
@@ -1,10 +1,28 @@
 public class Node<E> {
     private E data;
+    public static Node<E> next;
+    public static Node<E> previous;
     public Node(E d) {
         data = d;
     }
-    public static Node<E> next;
-    public static Node<E> previouse;
+    public E getValue() {
+        return data;
+    }
+    public void setValue(E d) {
+        data = d;
+    }
+    public void setNext(Node<E> n) {
+        next = n;
+    }
+    public E getNext() {
+        return next;
+    }
+    public void setPrevious(Node<E> p) {
+        previous = p;
+    }
+    public E getPrevious() {
+        return previous;
+    }
     public String toString() {
         String stingRingly = data.toString();
         return stingRingly;
